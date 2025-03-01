e8dc400762772d3096181ac6f12e38c3becf44da
koda <carloscoutinho1824@gmail.com>
2025-03-01 20:59:15 -0300

    calculos

---
+++ Projects/Calculations/main_01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //adiciono variaveis com numeros e somo eles dentro de outra variavel
+
+    int _num1 = 10;
+    int _num2 = 15;
+    int _result = _num1 + _num2;
+
+    printf("Number: %d \n", _num1);
+    printf("Number: %d \n", _num2);
+    printf("Result: %d \n", _result);
+
+    system("pause");
+
+    return 0;
+}
