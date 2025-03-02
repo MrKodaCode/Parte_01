57174eced94414f2c17e6117b07aa897f1dd65c4
koda <carloscoutinho1824@gmail.com>
2025-03-01 23:18:08 -0300

    ASCII

---
+++ Projects/ACSII/main_04.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char chr;
+
+    printf("Escreva Uma letra: ");
+
+    scanf("%c", &chr);
+
+    printf("Letra: %c. \n", chr);
+
+    printf("Valor em ACSII: %d. ", chr);
+
+    
+    return 0;
+}
