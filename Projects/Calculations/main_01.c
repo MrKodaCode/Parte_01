b812ef19407801dec71c7c124252f2fc237002bb
koda <carloscoutinho1824@gmail.com>
2025-03-01 22:50:13 -0300

    digitação

--- Projects/Calculations/main_01.c
+++ Projects/Calculations/main_01.c
@@ -5,15 +5,17 @@ int main()
 {
     //adiciono variaveis com numeros e somo eles dentro de outra variavel
 
-    int _num1 = 10;
-    int _num2 = 15;
-    int _result = _num1 + _num2;
+    int _num1           = 10;
+    int _num2           = 15;
+    int _result         = _num1 + _num2;
+
 
     printf("Number: %d \n", _num1);
     printf("Number: %d \n", _num2);
     printf("Result: %d \n", _result);
 
     system("pause");
+
 
     return 0;
 }
---
+++ Projects/digitar/main_02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int test;
+
+    printf("Digite Algo: ");
+
+    scanf("%d", &test);
+
+    printf("Digitado = %d", test);
+
+    return 0;
+}
